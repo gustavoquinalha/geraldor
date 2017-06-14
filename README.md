7be564336807996ec95c573730077ba2ad9e2d53
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-06-14 17:51:10 -0300

    :ok_hand:

---
+++ README.md
@@ -0,0 +1,9 @@
+# geraldor
+
+- https://pt.shopify.com/
+- https://topol.io/
+- https://mockflow.com/
+- https://flexbox.webflow.com/
+- https://preview.webflow.com/preview/flexbox-game?preview=d1a26b027c4803817087a91c651e321f&m=1
+- https://slides.com/
+- https://www.prezlr.com/
---
+++ src/main.js
@@ -0,0 +1,102 @@
+import Vue from 'vue'
+import { Compact } from 'vue-color'
+
+// Vue.component('todo-item', {
+//   template: `
+//     <li>
+//       {{ title }}
+//       <button v-on:click="$emit('remove')">X</button>
+//     </li>
+//   `,
+//   props: ['title']
+// })
+var defaultProps = {
+  hex: '#194d33',
+  hsl: {
+    h: 150,
+    s: 0.5,
+    l: 0.2,
+    a: 1
+  },
+  hsv: {
+    h: 150,
+    s: 0.66,
+    v: 0.30,
+    a: 1
+  },
+  rgba: {
+    r: 25,
+    g: 77,
+    b: 51,
+    a: 1
+  },
+  a: 1
+}
+
+new Vue({
+  el: '#app',
+  components: {
+    'compact-picker': Compact
+  },
+  data: {
+    topo: false,
+  center: true,
+  bottom: false,
+  justify: '',
+  align: '',
+    image: '',
+    colors: defaultProps,
+    text: 'Titulo',
+    stext: 'Subtitulo',
+    fontSize: '28',
+    fontSizes: '16',
+    counter: '',
+    shown: true,
+    shownn: true,
+    widthValue: 800,
+    heightValue: 600,
+    newTodoText: '',
+   todos: [
+     'Do the dishes',
+     'Take out the trash',
+     'Mow the lawn'
+   ]
+  },
+  methods: {
+
+    toggleText() {
+     this.shown = !this.shown;
+   },
+    toggleStext() {
+     this.shownn = !this.shownn;
+   },
+
+    addNewTodo: function () {
+      this.todos.push(this.newTodoText)
+      this.newTodoText = ''
+    },
+
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(files[0]);
+    },
+
+    createImage(file) {
+      var image = new Image();
+      var reader = new FileReader();
+      var vm = this;
+
+      reader.onload = (e) => {
+        vm.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+
+    removeImage: function (e) {
+      this.image = '';
+    }
+
+  }
+})
